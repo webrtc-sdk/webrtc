6be2fc29735f2b981efb3f4d047ae92f98a5ee6d
CloudWebRTC <duanweiwei1982@gmail.com>
2021-09-11 19:27:32 +0800

    Merge pull request #2 from webrtc-sdk/listen-only-audio-session
    allow listen-only mode in AudioUnit, adjust when category changes

--- sdk/objc/components/audio/RTCAudioSession.h
+++ sdk/objc/components/audio/RTCAudioSession.h
@@ -102,6 +102,9 @@ RTC_OBJC_EXPORT
 - (void)audioSession:(RTC_OBJC_TYPE(RTCAudioSession) *)audioSession
     audioUnitStartFailedWithError:(NSError *)error;
 
+/** Called when audio session changed from output-only to input & output */
+- (void)audioSessionWillRecord:(RTC_OBJC_TYPE(RTCAudioSession) *)audioSession;
+
 @end
 
 /** This is a protocol used to inform RTCAudioSession when the audio session
