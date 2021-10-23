85a34a5967d57480af8527e1674a9091575d7a29
David Liu <davidliu@deviange.net>
2021-10-23 15:33:55 +0900

    Use fallback encoder if primary can't be created In case if primary encoder can't be instantiated (max number of instances has reached, for example), use fallback encoder.
    Originally from:
    https://webrtc.googlesource.com/src/+/9b2a7461f099e3b732993925b24fd4752fd9c9a3

--- media/engine/simulcast_encoder_adapter.cc
+++ media/engine/simulcast_encoder_adapter.cc
@@ -684,15 +684,15 @@ void SimulcastEncoderAdapter::DestroyStoredEncoders() {
   }
 }
 
+
 std::unique_ptr<SimulcastEncoderAdapter::EncoderContext>
 SimulcastEncoderAdapter::FetchOrCreateEncoderContext(
     bool is_lowest_quality_stream) {
   bool prefer_temporal_support = fallback_encoder_factory_ != nullptr &&
                                  is_lowest_quality_stream &&
                                  prefer_temporal_support_on_base_layer_;
-
-  // Toggling of |prefer_temporal_support| requires encoder recreation. Find
-  // and reuse encoder with desired |prefer_temporal_support|. Otherwise, if
+  // Toggling of `prefer_temporal_support` requires encoder recreation. Find
+  // and reuse encoder with desired `prefer_temporal_support`. Otherwise, if
   // there is no such encoder in the cache, create a new instance.
   auto encoder_context_iter =
       std::find_if(cached_encoder_contexts_.begin(),
@@ -700,24 +700,45 @@ SimulcastEncoderAdapter::FetchOrCreateEncoderContext(
                      return encoder_context->prefer_temporal_support() ==
                             prefer_temporal_support;
                    });
-
   std::unique_ptr<SimulcastEncoderAdapter::EncoderContext> encoder_context;
   if (encoder_context_iter != cached_encoder_contexts_.end()) {
     encoder_context = std::move(*encoder_context_iter);
     cached_encoder_contexts_.erase(encoder_context_iter);
   } else {
-    std::unique_ptr<VideoEncoder> encoder =
+    std::unique_ptr<VideoEncoder> primary_encoder =
         primary_encoder_factory_->CreateVideoEncoder(video_format_);
+    std::unique_ptr<VideoEncoder> fallback_encoder;
     if (fallback_encoder_factory_ != nullptr) {
-      encoder = CreateVideoEncoderSoftwareFallbackWrapper(
-          fallback_encoder_factory_->CreateVideoEncoder(video_format_),
-          std::move(encoder), prefer_temporal_support);
+      fallback_encoder =
+          fallback_encoder_factory_->CreateVideoEncoder(video_format_);
     }
-
+    std::unique_ptr<VideoEncoder> encoder;
+    VideoEncoder::EncoderInfo primary_info;
+    VideoEncoder::EncoderInfo fallback_info;
+    if (primary_encoder != nullptr) {
+      primary_info = primary_encoder->GetEncoderInfo();
+      fallback_info = primary_info;
+      if (fallback_encoder == nullptr) {
+        encoder = std::move(primary_encoder);
+      } else {
+        encoder = CreateVideoEncoderSoftwareFallbackWrapper(
+            std::move(fallback_encoder), std::move(primary_encoder),
+            prefer_temporal_support);
+      }
+    } else if (fallback_encoder != nullptr) {
+      RTC_LOG(LS_WARNING) << "Failed to create primary " << video_format_.name
+                          << " encoder. Use fallback encoder.";
+      fallback_info = fallback_encoder->GetEncoderInfo();
+      primary_info = fallback_info;
+      encoder = std::move(fallback_encoder);
+    } else {
+      RTC_LOG(LS_ERROR) << "Failed to create primary and fallback "
+                        << video_format_.name << " encoders.";
+      return nullptr;
+    }
     encoder_context = std::make_unique<SimulcastEncoderAdapter::EncoderContext>(
         std::move(encoder), prefer_temporal_support);
   }
-
   encoder_context->encoder().RegisterEncodeCompleteCallback(
       encoded_complete_callback_);
   return encoder_context;
