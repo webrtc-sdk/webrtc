d3f6879e926d8231375435d3e11e025a32334545
Angelika Serwa <angelika.serwa@gmail.com>
2022-11-21 17:10:06 +0100

    Fix simulcast using hardware encoder on Android (#48)
    * Fix simulcast using hardware encoder on Android
    
    * Revert disabling apply_alignment_to_all_simulcast_layers
    
    * Update HardwareVideoEncoder.java

--- sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java
+++ sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java
@@ -45,8 +45,8 @@ class HardwareVideoEncoder implements VideoEncoder {
   private static final int MEDIA_CODEC_RELEASE_TIMEOUT_MS = 5000;
   private static final int DEQUEUE_OUTPUT_BUFFER_TIMEOUT_US = 100000;
 
-  // Size of the input frames should be multiple of 16 for the H/W encoder.
-  private static final int REQUIRED_RESOLUTION_ALIGNMENT = 16;
+  // Size of the input frames should be multiple of 2 for the H/W encoder.
+  private static final int REQUIRED_RESOLUTION_ALIGNMENT = 2;
 
   /**
    * Keeps track of the number of output buffers that have been passed down the pipeline and not yet
@@ -210,6 +210,11 @@ class HardwareVideoEncoder implements VideoEncoder {
     this.callback = callback;
     automaticResizeOn = settings.automaticResizeOn;
 
+    if (settings.width % REQUIRED_RESOLUTION_ALIGNMENT != 0
+        || settings.height % REQUIRED_RESOLUTION_ALIGNMENT != 0) {
+      Logging.e(TAG, "MediaCodec requires 2x2 alignment.");
+      return VideoCodecStatus.ERR_SIZE;
+    }
     this.width = settings.width;
     this.height = settings.height;
     useSurfaceMode = canUseSurface();
@@ -532,6 +537,12 @@ class HardwareVideoEncoder implements VideoEncoder {
     VideoCodecStatus status = release();
     if (status != VideoCodecStatus.OK) {
       return status;
+    }
+
+    if (newWidth % REQUIRED_RESOLUTION_ALIGNMENT != 0
+        || newHeight % REQUIRED_RESOLUTION_ALIGNMENT != 0) {
+      Logging.e(TAG, "MediaCodec requires 2x2 alignment.");
+      return VideoCodecStatus.ERR_SIZE;
     }
     width = newWidth;
     height = newHeight;
