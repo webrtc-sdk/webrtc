c0209ef4d07b51285457d11f038486a79f4c2d5c
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-12-06 15:09:32 +0900

    Stop recording on mute (turn off mic indicator) (#55)
    * initial impl
    
    * more comments
    
    * more comment
    
    * adjust indent
    
    * comments

--- audio/audio_state.h
+++ audio/audio_state.h
@@ -46,6 +46,8 @@ class AudioState : public webrtc::AudioState {
 
   void SetStereoChannelSwapping(bool enable) override;
 
+  void OnMuteStreamChanged() override;
+
   AudioDeviceModule* audio_device_module() {
     RTC_DCHECK(config_.audio_device_module);
     return config_.audio_device_module.get();
@@ -62,6 +64,9 @@ class AudioState : public webrtc::AudioState {
  private:
   void UpdateAudioTransportWithSendingStreams();
   void UpdateNullAudioPollerState();
+
+  // Returns true when at least 1 stream exists and all streams are not muted.
+  bool ShouldRecord();
 
   SequenceChecker thread_checker_;
   SequenceChecker process_thread_checker_;
