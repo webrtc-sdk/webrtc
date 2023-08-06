c8717a2d0a131119dfcd97584abf0e92f3845e13
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-08-06 11:18:32 +0900

    update header year

--- sdk/objc/base/RTCAudioRenderer.h
+++ sdk/objc/base/RTCAudioRenderer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 LiveKit
+ * Copyright 2023 LiveKit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
