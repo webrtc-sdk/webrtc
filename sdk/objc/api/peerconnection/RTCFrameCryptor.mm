ff694f8562b62eb471b6d9ec7a809ef0be87a1af
davidliu <davidliu@deviange.net>
2024-12-04 19:21:17 +0900

    Fix deadlock when creating a frame cryptor on iOS

--- sdk/objc/api/peerconnection/RTCFrameCryptor.mm
+++ sdk/objc/api/peerconnection/RTCFrameCryptor.mm
@@ -138,8 +138,8 @@ void RTCFrameCryptorDelegateAdapter::OnFrameCryptionStateChanged(const std::stri
             factory.signalingThread, [participantId stdString], mediaType,
             [self algorithmFromEnum:algorithm], keyProvider.nativeKeyProvider));
 
-    factory.workerThread->BlockingCall([self, nativeRtpSender] {
-      // Must be called on Worker thread
+    factory.signalingThread->BlockingCall([self, nativeRtpSender] {
+      // Must be called on signal thread
       nativeRtpSender->SetEncoderToPacketizerFrameTransformer(_frame_crypto_transformer);
     });
 
