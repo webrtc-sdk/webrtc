e62b2e61a52d347958ccdb4fde4e0dd9df14c9d5
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2026-03-30 14:41:27 +0200

    Fix recording always prepared mode (#229)

--- modules/audio_device/audio_device_buffer.cc
+++ modules/audio_device/audio_device_buffer.cc
@@ -105,8 +105,7 @@ int32_t AudioDeviceBuffer::RegisterAudioCallback(
   RTC_DCHECK_RUN_ON(&main_thread_checker_);
   RTC_DLOG(LS_INFO) << __FUNCTION__;
   if (playing_ || recording_) {
-    RTC_LOG(LS_ERROR) << "Failed to set audio transport since media was active";
-    return -1;
+    RTC_LOG(LS_WARNING) << "Setting audio transport while media is active";
   }
   audio_transport_cb_ = audio_callback;
   return 0;
